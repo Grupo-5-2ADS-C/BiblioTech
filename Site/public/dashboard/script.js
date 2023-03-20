60b5ca7923e884afeafaea52cad12e234359ee8d
DcoolerGuy <joao.penna@sptech.school>
2023-03-20 13:57:00 -0300

    correções e adição do modo noturno

---
+++ Site/public/dashboard/script.js
@@ -0,0 +1,9 @@
+const body = document.querySelector("body"),
+sidebar = document.querySelector(".side"),
+toggle = document.querySelector(".toggle"),
+modeSwitch = body.querySelector(".toggle-switch"),
+modeText = body.querySelector(".mode-text");
+
+modeSwitch.addEventListener("click", () =>{
+    body.classList.toggle("dark");
+})
